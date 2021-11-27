a7bd23128a557b566d8fc8c6eb7af334d2241bda
github username <cmyerskeitt@gmail.com>
2021-11-26 19:20:54 -0500

    add background image and resize

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css'
 import Clock from "./Clock"
+import background from './CongratsBackground.jpg'
 
 class App extends Component { 
     constructor (props) {
@@ -15,15 +16,20 @@ class App extends Component {
         this.setState({deadline: this.state.newDeadline})
     }
 
+    convertDeadline(){
+
+    }
+
 
     render () {
         return (
-            <div className="App">
+            <div className="App" style={{ backgroundImage: `url(${background})`, backgroundPosition: 'center', backgroundSize: 'cover'}}>
             <div className="App-title"> Countdown to {this.state.deadline} </div>
             <Clock deadline={this.state.deadline}/>
             <div>
-            <input placeholder='Enter New Date' onChange={event => this.setState({newDeadline: event.target.value})}/>
-            <button onClick={()=> this.changeDeadline()}> Submit</button>
+                <input type= "text" placeholder='Enter New Date' onChange={event => this.setState({newDeadline: event.target.value})}/>
+                <br/>
+                <button type="submit" class="btn btn-primary btn-lg" onClick={()=> this.changeDeadline()}> Submit</button>
             </div>
             </div>
         )
